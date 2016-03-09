1bb1c44ba291ba295cbd33579fd9e3dc5365c4fa
bgtest001 <q1970633640@gmail.com>
2016-03-09 21:06:32 +0800

    123

--- lab02_02/main.cpp
+++ lab02_02/main.cpp
@@ -2,26 +2,29 @@
 #include <string>
 #include <fstream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 using namespace std;
 
-int main()
+int main(int argc,char *argv[])
 {
-    char c[100];
     ifstream fin;
-    scanf("%s",c);
-    fin.open(c);
-
+    fin.open(argv[1]);
     string a;
     string b;
     getline(fin,a) ;
     getline(fin,b) ;
-
     int x=0,i;
     for(i=0; i<=a.length()-2; i++)
     {
-        if(i == a.find(b, i )  )//从位置x开始查找字符串“eeefff“在当前字符串中第一次出现的位置
+        if(i == a.find(b, i )  )
             x++;
     }
     cout<<x<<endl;
-    return 0;
+
+ cout<<"Process returned 0 (0x0)   execution time : 0.000 s\nPress any key to continue."<<endl;
+     char cc;
+         cc=getch();
+     return 0;
+
 }

f56b37be0ac08f1fb6fcfb1e6cede26516b01ead
bgtest001 <q1970633640@gmail.com>
2016-03-09 14:13:13 +0800

    123

--- 3119/main.cpp
+++ 3119/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<int> a;
+    int i,t;
+    for(i=0; i<3; i++)
+    {
+        cin>>t;
+        a.push_back(t);
+    }
+    sort(a.begin(),a.end());
+    for(i=0; i<2; i++)
+    {
+        cout<<a[i]<<' ';
+    }
+    cout<<a[i]<<endl;
     return 0;
 }

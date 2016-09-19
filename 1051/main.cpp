9b4bb334463563db8ab6d03b46271b3612ba58d2
1970633640 <q1970633640@gmail.com>
2016-09-19 17:13:22 +0800

    123

---
+++ 1051/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+//long long int zui da 9223372036854775807
+using namespace std;
+
+int n;
+
+long long int f(int *p)
+{
+    long long int m[51][51]= {0};
+long long int s[51][51]= {0};
+    int i,j,L,k;
+    for(i=1; i<=n; i++)m[i][i]=0;
+    for(L=2; L<=n; L++)
+    {
+        for(i=1; i<=n-L+1;i++)
+        {
+            j=i+L-1;
+            m[i][j]=9223372036854775807;
+            for(k=i;k<=j-1;k++)
+            {
+                long long int q=m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j];
+                if(q<m[i][j]){m[i][j]=q; s[i][j]=k;}
+            }
+        }
+    }
+
+return m[1][n];
+}
+
+
+int main()
+{
+    int ii,i,nn;
+    cin>>nn;
+    for(ii=0; ii<nn; ii++)
+    {
+        int p[52];
+        cin>>n;
+        for(i=1; i<=n; i++)
+            cin>>p[i-1]>>p[i];
+        cout<<f(p)<<endl;
+    }
+    return 0;
+}
--- 1851/main.cpp
+++ 1851/main.cpp
@@ -4,19 +4,30 @@ using namespace std;
 
 int main()
 {
-   long long int x,x1,x2;
-   int cows,cow[50000],n,i;
-   cin>>cows>>n;
-for(i=0;i<cows;i++)cin>>cow[i];
-   x1=0; x2=1;
-   for(i=1;i<n;i++){x=(2*x1+x2)%987654321;x1=x2;x2=x;}//cout<<x<<"-";
-   if (n % 2 !=0){long long int sum=0;for(i=0;i<cows;i++)sum+=(x*cow[i]);
-   for(i=0;i<cows;i++)cout<<(sum-cow[i])%987654321<<endl;
-   }
-   else{
-    long long int sum=0;for(i=0;i<cows;i++)sum+=(x*cow[i]);
-   for(i=0;i<cows;i++)cout<<(sum+cow[i])%987654321<<endl;
+    long long int x,x1,x2;
+    int cows,cow[50000],n,i;
+    cin>>cows>>n;
+    for(i=0; i<cows; i++)cin>>cow[i];
+    x1=0;
+    x2=1;
+    for(i=1; i<n; i++)
+    {
+        x=(2*x1+x2)%987654321;    //cout<<x<<"-";
+        x1=x2;
+        x2=x;
+    }
+    if (n % 2 !=0)
+    {
+        long long int sum=0;
+        for(i=0; i<cows; i++)sum+=(x*cow[i]);
+        for(i=0; i<cows; i++)cout<<(sum-cow[i])%987654321<<endl;
+    }
+    else
+    {
+        long long int sum=0;
+        for(i=0; i<cows; i++)sum+=(x*cow[i]);
+        for(i=0; i<cows; i++)cout<<(sum+cow[i])%987654321<<endl;
 
-   }
+    }
     return 0;
 }

338370e28e01f3d7b125caefd00c91bb95f7991b
1970633640 <q1970633640@gmail.com>
2016-09-20 12:33:25 +0800

    123

--- 1806/main.cpp
+++ 1806/main.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 int main()
-{int n,i,j,x[1025];
-    cin n;
-    for(i=0;i<n;i++)cin>>x[i];
-    for(i=0;i<n-1;i++){
+{
+    int n,i,j,x[1025];
+    cin>>n;
+    for(i=0; i<n; i++)cin>>x[i];
+    for(i=0; i<n-1; i++)
+    {
         int temp=x[i],m=i;
-        for(j=i;j<n;j++)if(x[j]<temp){temp=x[j];m=j;}
-
-
+        for(j=i; j<n; j++)if(x[j]<temp)
+            {
+                temp=x[j];
+                m=j;
+            }
+        swap(x[i],x[m]);
     }
+    for(i=0; i<n-1; i++)cout<<x[i]<<" ";
+    cout<<x[i]<<endl;
     return 0;
 }
